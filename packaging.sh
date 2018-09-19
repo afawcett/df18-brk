6f88bf1334f6cc46f9d791ebfe4a94e60b819e33
Andrew Fawcett <afawcett@afawcett-ltm.internal.salesforce.com>
2018-09-18 21:48:25 -0700

    Added some sleeps

--- packaging.sh
+++ packaging.sh
@@ -8,6 +8,7 @@ rm -rf ./dreamhouse-sfdx
 dprintf "git clone https://github.com/dreamhouseapp/dreamhouse-sfdx"
 dprintf "cd dreamhouse-sfdx"
 dprintfake "sfdx force:package:create -n Dreamhouse -t Unlocked -r force-app"
+sleep 2
 echo sfdx-project.json has been updated.
 echo Successfully created a package. 0HoB0000000KyjVKAS
 echo === Ids
@@ -15,12 +16,14 @@ echo NAME        VALUE
 echo ──────────  ──────────────────
 echo Package Id  0HoB0000000KyjVKAS
 dprintfake "sfdx force:package:version:create - Dreamhouse -k password123 -w 10"
+sleep 5
 echo sfdx-project.json has been updated.
 echo Successfully created the package version [08cB0000000KyjLIAS]. Subscriber Package Version Id: 04tB0000000Y0DJIA0.
 echo Package Installation URL: https://login.salesforce.com/packaging/installPackage.apexp?p0=04tB0000000Y0DJIA0
 echo As an alternative, you can use the "sfdx force:package:install" command.
 dprintf "code ."
 dprintfake "sfdx force:package:install --package Dreamhouse@1.0.0-1 -u Sandbox -k password123 -w 10"
+sleep 2
 echo This package might send or receive data from these third-party websites:
 echo 
 echo api.metamind.io
@@ -29,6 +32,7 @@ echo dreamhouzz-push-server.herokuapp.com
 echo hooks.slack.com
 echo
 echo Grant access y/n?: y
+sleep 5
 echo Successfully installed package [04tB0000000Y0DJIA0]
 dprintf "sfdx force:org:open -u Sandbox -p /lightning/setup/ImportedPackage/home"
 cd ..
