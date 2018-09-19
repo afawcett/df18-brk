9d93e29cc3f0de91bdc8865ea63823d269ebade4
Andrew Fawcett <afawcett@afawcett-ltm.internal.salesforce.com>
2018-09-18 21:39:00 -0700

    Added vscode step

--- packaging.sh
+++ packaging.sh
@@ -19,6 +19,7 @@ echo sfdx-project.json has been updated.
 echo Successfully created the package version [08cB0000000KyjLIAS]. Subscriber Package Version Id: 04tB0000000Y0DJIA0.
 echo Package Installation URL: https://login.salesforce.com/packaging/installPackage.apexp?p0=04tB0000000Y0DJIA0
 echo As an alternative, you can use the "sfdx force:package:install" command.
+dprintf "code ."
 dprintfake "sfdx force:package:install --package Dreamhouse@1.0.0-1 -u Sandbox -k password123 -w 10"
 echo This package might send or receive data from these third-party websites:
 echo 
