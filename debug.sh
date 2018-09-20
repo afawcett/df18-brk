9fcbb43594479dcb11633eb35b6d6cc3c082fa73
Andrew Fawcett <afawcett@afawcett-ltm.internal.salesforce.com>
2018-09-20 11:46:49 -0700

    Added cleanup steps to force debug, editing and testing demos down scratch org path

--- debug.sh
+++ debug.sh
@@ -2,7 +2,20 @@
 source ./autopilot.sh
 
 # Cleanup
+echo Demo Setup: In Progress...
+rm -rf ./dreamhouse-sfdx
+git clone -q https://github.com/dreamhouseapp/dreamhouse-sfdx > /dev/null
+cd ./dreamhouse-sfdx
+sfdx force:org:create -f config/project-scratch-def.json -s -a mydhdev > /dev/null
+echo Demo Setup: In Progress...
+sfdx force:source:push > /dev/null
+echo Demo Setup: Complete
+cd ..
+
 
 # Demo script
+dprintf "git clone https://github.com/dreamhouseapp/dreamhouse-sfdx"
 dprintf "cd ./dreamhouse-sfdx" 
+dprintf "sfdx force:org:create -f config/project-scratch-def.json -s -a mydhdev"
+dprintf "sfdx force:source:push"
 dprintf "code ."
--- testing.sh
+++ testing.sh
@@ -2,6 +2,15 @@
 source ./autopilot.sh
 
 # Cleanup
+echo Demo Setup: In Progress...
+rm -rf ./dreamhouse-sfdx
+git clone -q https://github.com/dreamhouseapp/dreamhouse-sfdx > /dev/null
+cd ./dreamhouse-sfdx
+sfdx force:org:create -f config/project-scratch-def.json -s -a mydhdev > /dev/null
+echo Demo Setup: In Progress...
+sfdx force:source:push > /dev/null
+echo Demo Setup: Complete
+cd ..
 
 # Demo script
 dprintf "cd ./dreamhouse-sfdx" 
